11b6e1f3a0680d643a83da84839a6cba1cff4ba6
Ali Naseri <mo.ali.naseri@gmail.com>
2024-07-31 13:08:28 +0330

    fix: change config to fix build errors

--- app/main.tsx
+++ app/main.tsx
@@ -1,13 +1,16 @@
 import '@assets/styles/main.css';
-import { CoreLayout, CoreProvider } from '@core';
 
+import { CoreLayout, CoreProvider } from '@core';
+import { Button } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CoreProvider>
-      <CoreLayout>App</CoreLayout>
+      <CoreLayout>
+        <Button>button</Button>
+      </CoreLayout>
     </CoreProvider>
   </React.StrictMode>,
 );
